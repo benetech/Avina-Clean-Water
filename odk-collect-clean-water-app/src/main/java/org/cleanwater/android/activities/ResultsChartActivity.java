c6535ca1bb8e66596d32e61c4829004be7370313
Nima A <nimaa@benetech.org>
2015-05-26 17:08:01 -0400

    New Avina clearwater android app.

---
+++ odk-collect-clean-water-app/src/main/java/org/cleanwater/android/activities/ResultsChartActivity.java
@@ -0,0 +1,29 @@
+package org.cleanwater.android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+
+import org.odk.collect.android.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by animal@martus.org on 5/22/15.
+ */
+public class ResultsChartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        setContentView(R.layout.results_activity);
+//        System.out.println("--------------------------------------------------------------------------------- on See");
+//        LineChart chart = (LineChart) findViewById(R.id.results_chart);
+//        chart.invalidate();
+//        LineData lineData = new LineData(new ArrayList());
+//        chart.setData(lineData);
+    }
+}
